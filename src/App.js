e3553a8b4ef7ea03e6f0513bcd2646ef08ab30f3
Darshan  Bajgain <70639590+darshanbajgain@users.noreply.github.com>
2023-08-29 13:27:05 +0000

    Some advanced styling added based on logics

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ function App() {
 
   const [count, setCount] = useState(0);
   const [message, setMessage] = useState(" ");
+  const [isVisible, setIsVisible] = useState(false);
   const changeValue = () => {
     setCount(count + 1);
     setMessage(" ");
+    setIsVisible(false);
   }
   const resetValue = () => {
     setCount(0)
     if (count === 0) {
       setMessage("Value is already in default state.i.e '0'");
+      setIsVisible(true);
     }
   }
 
@@ -29,7 +32,7 @@ function App() {
         <button className="bg-white font-extrabold text-5xl font-montserrat text-cyan-400  m-5 px-8 py-4 rounded-3xl hover:bg-cyan-400 hover:text-white   transition 500" onClick={resetValue} >
           reset
         </button>
-        <p className='text-red-700 text-lg'>
+        <p className= {isVisible?'transition 400 flex m-auto w-[80%] justify-center text-center px-6 py-8 bg-white rounded-2xl text-red-700 text-lg': 'none'}>
           {message}
         </p>
       </div>
