5c62fc434482ca069f79e33581de1102543ed3f8
Darshan Bajgain <darshanbajgain90@gmail.com>
2023-08-29 18:10:44 +0545

    header updated

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
 
   return (
     <div className=' block mx-20'>
+      <h1 className='text-black text-center font-extrabold text-3xl uppercase m-30 p-16'>
+        Count-Reset App
+      </h1>
       <div className=" flex flex-col rounded-3xl justify-center items-center p-10 m-auto w-[50%] max-w-[400] max-h-[400] bg-slate-500">
         <h1 className="text-white text-2xl font-semibold"> {count} </h1>
         <button className="bg-white font-extrabold text-5xl font-montserrat text-cyan-400  m-5 px-8 py-4 rounded-3xl hover:bg-cyan-400 hover:text-white  transition 500" onClick={changeValue} >
